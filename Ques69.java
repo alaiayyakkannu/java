9b1765ca9e409d1f9e6dac370fd0c29c45090623
Alai A I <40653491+alaiayyakkannu@users.noreply.github.com>
2018-08-01 10:28:27 +0530

    Add files via upload

---
+++ Ques69.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+ public class Ques69 {
+ public static void main(String[] args)
+ {
+    String main_string = "Deva  Ganesh";    
+    System.out.println(main_string.substring(0, main_string.length()/2));	
+  } 
+ }
---
+++ Ques85.java
@@ -0,0 +1,9 @@
+import java.util.*; 
+import java.io.*; 
+ public class Ques85 {
+ public static void main(String[] args)
+ {
+   String string1 = "Hello This is Devaganesh";
+    System.out.println(string1.startsWith("Hello"));
+  }
+}
